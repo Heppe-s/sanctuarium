37997d8754d85441d2685ff4a2022cc2428bac71
Guilherme Felipe <glipecoelho2@gmail.com>
2024-03-28 19:54:47 -0300

    feat: new imports and modification
    -import and implement EditorJS and dependencies
    -import and implement Tailwind
    -switch button for theme (dark mode)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./globals.css";
+import { ThemeProvider } from "./components/theme-provides";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
+
 );
