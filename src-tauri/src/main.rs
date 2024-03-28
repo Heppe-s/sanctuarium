b07040da0043c5e9456f4f449c4e56d96a01e29f
Guilherme Felipe <glipecoelho2@gmail.com>
2024-03-28 19:56:16 -0300

    feat: sqlite db install

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,11 +6,18 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+mod db;
 
 fn main() {
     tauri::Builder::default()
+        .setup(|_app| {
+            // Initialize the database.
+            db::init();
+
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while running tauri application");      
 }
