b6d7862d4564764cc17c64e108b9e16a2361727a
Pedro Ryan <prgames1234@gmail.com>
2024-04-05 21:41:39 -0300

    style: remove all margins and fix icon

--- src/components/ui/citation.tsx
+++ src/components/ui/citation.tsx
@@ -1,26 +1,23 @@
+import QuoteIcon from "@/components/quote";
 import Typography from "@/components/ui/typography";
 import "@/globals.css";
 import { cn } from "@/lib/utils";
-import QuoteIcon from "../quote";
 
-export default function Citation({ className }: { className: string }) {
+export default function Citation({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "bg-slate-500 text-white mt-8 w-10/12 h-1/5 rounded-xl",
+        "bg-slate-500 text-white w-full h-auto rounded-xl relative pt-14 pb-10 px-16",
         className
       )}
     >
-      <QuoteIcon className="bg-clip-padding w-16 h-16 m-2 absolute rotate-180" />
-      <Typography variant={"h4"} className=" ml-24 pt-10 pr-4 relative">
+      <QuoteIcon className="w-11 h-11 left-4 top-2 absolute rotate-180" />
+      <Typography variant="h4" className="">
         O verdadeiro teste não é se você evitará esse fracasso, porque não o
         fará. É se você deixa que isso endureça ou envergonhe-o pela inércia, ou
         se você aprende com ele; se você escolhe perseverar.”
       </Typography>
-      <Typography
-        variant={"p"}
-        className="pr-10 italic opacity-70 pb-2 relative text-right"
-      >
+      <Typography variant="p" className="italic text-right">
         - Barack Obama
       </Typography>
     </div>
