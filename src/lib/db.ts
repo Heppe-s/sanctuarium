46b804e172451607dad6ac1bc8ecb1b67b700429
Pedro Ryan <prgames1234@gmail.com>
2024-04-10 01:32:04 -0300

    feat: create function to get annotations in database with pagination

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,6 +11,21 @@ interface AnnotationInfos {
   text?: string;
   file?: string;
 }
+
+interface Annotation extends AnnotationInfos {
+  id: number;
+  last_modify: Date;
+  create_at: Date;
+  line_num: number;
+  categories?: [
+    {
+      id: number;
+      name: string;
+      description?: string;
+    }
+  ];
+}
+
 let db: Database;
 
 export async function init() {
@@ -61,4 +76,41 @@ export function addCategory(annotationId: number, categoryId: number) {
 
   const query = `INSERT INTO annotations_categories (annotation_id, category_id) VALUES ($1, $2);`;
   return db.execute(query, [annotationId, categoryId]);
+}
+
+export async function getAnnotations(page: number = 1, limit: number = 10) {
+  if (page <= 0 || limit <= 0) {
+    throw new Error("Page and limit must be positive integers");
+  }
+
+  const query = `
+  WITH AnnotationsNumeradas AS (
+    SELECT 
+      a.id,
+      a.title,
+      a.text,
+      a.file,
+      a.last_modify,
+      a.create_at,
+      JSON_GROUP_ARRAY(
+        JSON_OBJECT(
+          'id', c.id,
+          'name', c.name,
+          'description', c.description
+        )
+      ) as categories,
+      ROW_NUMBER() OVER (ORDER BY a.last_modify DESC) as line_num
+    FROM annotations a
+    LEFT JOIN annotations_categories ac ON a.id = ac.annotation_id
+    LEFT JOIN categories c ON ac.category_id = c.id
+    GROUP BY a.id, a.title, a.text, a.file, a.last_modify, a.create_at
+  )
+  SELECT *
+  FROM AnnotationsNumeradas
+  WHERE line_num BETWEEN $1 AND $2;`;
+
+  const start = page > 1 ? page * limit : page;
+  const end = start + limit;
+
+  return db.select<Annotation[]>(query, [start, end]);
 }
