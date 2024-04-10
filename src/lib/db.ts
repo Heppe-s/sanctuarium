2e3713a3a009cc0700a7e9ac4ae14a64c3aa8b56
Pedro Ryan <prgames1234@gmail.com>
2024-04-10 01:27:45 -0300

    feat: create function to create annotation in database

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,10 @@
 import { invoke } from "@tauri-apps/api/core";
 import Database from "@tauri-apps/plugin-sql";
+interface AnnotationInfos {
+  title: string;
+  text?: string;
+  file?: string;
+}
 let db: Database;
 
 export async function init() {
@@ -7,4 +12,24 @@ export async function init() {
 
   const config = await invoke<{ db_url: string }>("get_config");
   db = await Database.load(config.db_url);
+}
+
+export async function createAnnotation(annotation: AnnotationInfos) {
+  if (!annotation.title) throw new Error("No title has provided to annotation");
+  const { title, file, text } = annotation;
+
+  if (file && typeof file !== "string") {
+    throw new Error("File must be a string");
+  }
+
+  if (text && typeof text !== "string") {
+    throw new Error("Text must be a string");
+  }
+
+  const query =
+    "INSERT INTO annotations (title, file, text) VALUES ($1, $2, $3)";
+
+  await db.execute(query, [title, file, text]);
+
+  return true;
 }
